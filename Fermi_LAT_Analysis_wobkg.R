ad9ee8194af99fdf04b96cf68f7398593502ba01
baner175 <baner175@umn.edu>
2026-04-15 16:33:19 -0500

    adding detailed documentation

--- Fermi_LAT_Analysis_wobkg.R
+++ Fermi_LAT_Analysis_wobkg.R
@@ -3,8 +3,8 @@ rm(list = ls())
 ################################################################################
 ################################################################################
 ##                                                                            ##
-## Code needed to reproduce the analysis of the simulated data from           ##
-## Fermi-LAT without a background-only sample                                 ##
+##     Code for reproducing the analysis on the simulated data from           ##
+##     Fermi-LAT without a background-only sample                             ##
 ##                                                                            ##
 ################################################################################
 ################################################################################
@@ -58,7 +58,7 @@ M_lower <- log(mean_sig) - r # lower bound of the signal region
 M_upper <- log(mean_sig) + r # upper bound of the signal region
 
 
-# negative log-likelihood using the nominal background model
+# negative log-likelihood using the benchmark background model q_\alpha:
 q_model <- function(alpha){
   q_mass <- (1/alpha)*((l+1)^(-alpha) - (u+1)^(-alpha))
   q_i <- sapply(y, function(t){
@@ -70,7 +70,7 @@ alpha_hat <- nlminb(start = 0.01,
                    objective = q_model,
                    upper = 10, lower = 0)$par # MLE of alpha
 
-# defining q_alpha at MLE:
+# defining q_\alpha at \alpha = \hat\alpha:
 q <- function(x) {
   q_mass <- (1/alpha_hat)*((l+1)^(-alpha_hat) - (u+1)^(-alpha_hat))
   return(((x+1)^(-alpha_hat-1))/q_mass)
@@ -81,52 +81,58 @@ q <- function(x) {
 mean1_in_g <- (M_lower + log(mean_sig))/2 
 mean2_in_g <- (M_upper + log(mean_sig))/2
 sig_fs <- sqrt(integrate(function(x) {(x^2)*fs(x)}, l, u)$value - integrate(function(x) {(x)*fs(x)}, l, u)$value^2)
-sig_0 <- 3*sig_fs # SD for the gaussian components
+sig_0 <- 3*sig_fs # SD for the Gaussian components
 
 # Defining proposal background g:
 g <- function(x, lambda){
   phi_1 <- dtrunc(x, spec = 'norm', a = l, b = u,
-                    mean = mean1_in_g, sd = sig_0)
+                    mean = mean1_in_g, sd = sig_0) # first Gaussian component in g
   phi_2 <- dtrunc(x, spec = 'norm', a = l, b = u,
-                    mean = mean2_in_g, sd = sig_0)
+                    mean = mean2_in_g, sd = sig_0) # second Gaussian component in g
   return(
-    lambda*(phi_1 + phi_2) + (1-2*lambda)*q(x)
+    lambda*(phi_1 + phi_2) + (1-2*lambda)*q(x) # benchmark model q mixed with Gaussian components
   )
 }
 
-d_log_h <- function(t) -log(t+1)
-E_q_d_log_h <- integrate(function(t) d_log_h(t)*q(t), l, u)$value
-d_log_q <- function(t) d_log_h(t) - E_q_d_log_h
+# We let h be the un-scaled version of q(x,\alpha) such that q(x,\alpha) = h(x,\alpha)/integrate(h,l,u)
+# We now define different components for calculating the denominator of the test statistic
+
+d_log_h <- function(t) -log(t+1) # derivative of log h(x, \alpha) w.r.t. alpha
+E_q_d_log_h <- integrate(function(t) d_log_h(t)*q(t), l, u)$value # E_q(log h(x, \alpha))
+d_log_q <- function(t) d_log_h(t) - E_q_d_log_h # derivative of log q(x, \alpha) w.r.t. alpha
 d2_log_q_int1 <- integrate(function(t) {
   (d_log_h(t)^2)*q(t)
 }, l, u)$value
 d2_log_q <- -d2_log_q_int1 + E_q_d_log_h^2
+
+# Defining a function that performs the signal search for each value of \lambda in g:
 signal_search <- function(lambda){
   norm_S <- integrate(function(x){
     return(((fs(x)/g(x, lambda = lambda) - 1)^2)*g(x, lambda = lambda))
-  }, l, u)$value |> sqrt()
+  }, l, u)$value |> sqrt() # calculating ||S||_G
   
-  S0_vec <- sapply(y, function(x){
+  S0_vec <- sapply(y, function(x){ # Evaluating S_0 on the physics data
     S_val <- (fs(x)/g(x, lambda = lambda) - 1)
     return(S_val/(norm_S^2))
   })
-  theta0_hat <- mean(S0_vec)
-  d_log_q_vec <- sapply(y, d_log_q)
+  theta0_hat <- mean(S0_vec) # \hat{\theta}_0 : conservative estimate of \eta
+  d_log_q_vec <- sapply(y, d_log_q) # evaluating d_log_q on the physics data
   d_normS_sq <- -(1-2*lambda)*integrate(function(y){
     q <- q(y)
     fs <- fs(y)
     g <- g(y, lambda = lambda)
     d_log_q <- d_log_q(y)
     return(((fs/g)^2)*q*d_log_q)
-  }, l, u)$value
+  }, l, u)$value # derivative of ||S||_G w.r.t. \alpha
   
-  d_S0_vec <- sapply(y, function(y){
+  d_S0_vec <- sapply(y, function(y){ # Evaluating d_alpha_S0 on the physics data
     fs <- fs(y)
     q <- q(y)
     g <- g(y, lambda = lambda)
     d_log_q <- d_log_q(y)
     return(-((norm_S^2)*(fs/(g^2))*(1-2*lambda)*q*d_log_q + (fs/g-1)*d_normS_sq)/(norm_S^4))
   })
+  # components for sig_theta0:
   V_hat <- mean(d_log_q_vec^2)
   J_hat <- -d2_log_q
   d_theta0 <- mean(d_S0_vec)
@@ -142,10 +148,10 @@ signal_search <- function(lambda){
            p_val))
 }
 
-# lambda_max <- 0.05 # change lambda_max here
-lambda_seq <- c(0.03, 0.05, 0.07)
+lambda_seq <- c(0.03, 0.05, 0.07) # candidate values for \lambda
 
-res_sig_search <- sapply(lambda_seq, signal_search)
+res_sig_search <- sapply(lambda_seq, signal_search) # signal_search on each value of \lambda
+# Results table:
 res_sig_search <- cbind(lambda_seq, t(res_sig_search))
 res_sig_search <- data.frame(res_sig_search)
 colnames(res_sig_search) <- c("lambda", "theta0beta_hat", "p-value")
